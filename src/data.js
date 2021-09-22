0e5f4441618b3a8f297767ba388b9273950db8c5
Jamie Kuras <jamiekuras@gmail.com>
2021-09-21 20:23:21 -0400

    updated package.json

--- src/data.js
+++ src/data.js
@@ -1,35 +1,51 @@
 export const projects = [
   {
-    title: "React Reserve",
-    subtitle: "MERN Stack",
+    title: "Sip",
+    subtitle: "API Web App",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-1.gif",
-    link: "https://reactbootcamp.com",
+      "SIP, or Spirit Index Program, is a web application that utilizes TheCocktailDB API and Yelp Fusion API to search for cocktails using a base liquor (vodka, rum, gin, whisky, etc.) as well as searching for local bars that serve cocktails if you don't feel like making a cocktail at home!",
+    image: "./sip.jpg",
+    link: "https://danshoup.github.io/build-a-cocktail/",
   },
   {
-    title: "React Tracks",
-    subtitle: "React and Python",
+    title: "Redux Store",
+    subtitle: "React and MongoDB",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-2.gif",
-    link: "https://reactbootcamp.com",
+      "E-commerce website that allows user to login and purchase items, also see their order history.",
+    image: "./reduxStore.jpg",
+    link: "https://floating-hollows-75339.herokuapp.com/",
   },
   {
-    title: "DevChat",
-    subtitle: "React and Firebase",
+    title: "GameOn",
+    subtitle: "Full MERN App",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-3.gif",
-    link: "https://reactbootcamp.com",
+      "Tasked with building a collaborative, Full-stack MERN appication, Game-ON, is an online 'spit-and-handshake' sports competitive app.",
+    image: "./gameOn.jpg",
+    link: "https://game-on-msu-2021.herokuapp.com/",
   },
   {
-    title: "Epic Todo App",
-    subtitle: "React Hooks",
+    title: "Code Quiz",
+    subtitle: "Javascript",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-4.gif",
-    link: "https://reactbootcamp.com",
+      "A timer-based coding assessment using vanilla JavaScript",
+    image: "./codeQuiz.jpg",
+    link: "https://jkur16.github.io/Code-Quiz/",
+  },
+  {
+    title: "Online/Offline Budget Tracker",
+    subtitle: "NodeJS and ExpressJS",
+    description:
+      "The user will be able to add expenses and deposits to their budget with or without a connection. A user will also be able to download the app since it is a progressive web app. When entering transactions offline, they should populate the total when brought back online.",
+    image: "./Online-Offline Budget.jpg",
+    link: "https://immense-beyond-60421.herokuapp.com/",
+  },
+  {
+    title: "Weather Dashboard",
+    subtitle: "Javascript",
+    description:
+      "Users are able to enter a city name in the search box and have current weather conditions displayed for that city, as well a five day forecast.",
+    image: "./weatherdashboard.jpg",
+    link: "https://jkur16.github.io/Code-Quiz/",
   },
 ];
 
